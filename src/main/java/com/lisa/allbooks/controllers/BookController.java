5cfdf3600c3db2a5d5c371be193758e812f0cece
Lisa <lisa.hl.chen@gmail.com>
2022-06-05 19:31:26 -0700

    Listing all

---
+++ src/main/java/com/lisa/allbooks/controllers/BookController.java
@@ -0,0 +1,77 @@
+package com.lisa.allbooks.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lisa.allbooks.models.Book;
+import com.lisa.allbooks.services.BookService;
+
+@Controller //Controller will return a jsp page
+public class BookController {
+	
+	@Autowired
+	BookService bookService;
+	
+	@RequestMapping("/books")
+	public String index(Model model) {
+		List<Book> books = bookService.allBooks();
+		model.addAttribute("allBooks",books);
+		return "index.jsp";
+ }
+ 
+//	@RequestMapping(value="/books", method=RequestMethod.POST)
+// public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+//     Book book = new Book(title, desc, lang, numOfPages);
+//     return bookService.createBook(book);
+// }
+ 
+ @RequestMapping("/books/{id}")
+ public String show(Model model, @PathVariable("id") Long id) {
+     Book book = bookService.findBook(id);
+     model.addAttribute("showBook",book);
+     
+     return "show.jsp";
+ }
+//
+// @RequestMapping(value="/books/{id}", method=RequestMethod.PUT)
+// public Book update(@PathVariable("id") Long id, @RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+//	 
+//	 Book updatedBook = bookService.findBook(id);
+//	 
+//	 // calling a method from you Book class
+//	 updatedBook.setTitle(title);
+//	 updatedBook.setDescription(desc);
+//	 updatedBook.setLanguage(lang);
+//	 updatedBook.setNumberOfPages(numOfPages);
+//
+//	 Book book = bookService.updateBook(updatedBook);
+//	 return book;
+// }
+ 
+ 
+ // COOL METHOD BONDING TIME WITH SIERRA (less calls to database)
+// @RequestMapping(value="/books/{id}", method=RequestMethod.PUT)
+// public Book update(@PathVariable("id") Long id, @RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+//	 
+//	 // setting up to query - describing the row you want to update
+//	 Book updatedBook = new Book(title, desc, lang, numOfPages);
+//	 
+//	 // calling the setID method on the book
+//	 updatedBook.setId(id);
+//
+//	 Book book = bookService.updateBook(updatedBook);
+//	 return book;
+// }
+// 
+ 
+ 
+// @RequestMapping(value="/books/{id}", method=RequestMethod.DELETE)
+// public void destroy(@PathVariable("id") Long id) {
+//     bookService.deleteBook(id); 
+// }
+}
